3ed2be9175fbadfcc37de92e06ba3a88452bb187
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2021-01-06 18:31:04 +0900

    ログ削除

--- viewer.js
+++ viewer.js
@@ -1,9 +1,7 @@
-console.log('aabbba')
-
 const Serializer = require('@zharktas/vdom-serialize');
 const socket = io();
 socket.on("broadcast", (data) => {
-  console.log("received sync event", data);
+  console.log("received sync event", data.vdom);
   var serialized = Serializer.deserializePatches(data.vdom);
   patch(document.body, serialized);
 });
