bcb2c7dc8454c03ce4e8de85e54a22c43777e821
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2021-08-11 23:13:43 +0900

    targetが1オブジェクトの場合の動作確認

--- js/helper.js
+++ js/helper.js
@@ -1,91 +1,53 @@
 const setKey = function (vdom, vdomWithKey) {
-    console.log('vdomWithKey=', vdomWithKey)
+    // console.log('vdomWithKey=', vdomWithKey)
     console.log('vdom=', vdom)
     result = walk(vdom, vdomWithKey)
-    console.log('result=', result)
+    console.log('vdom=', vdom)
 
     return { beforeVdom: 'aaa', afterVdom: 'bbbb'};
 };
+
 // target ...これを探す
 // source ...keyあり、検索対象
 function walk(target, source){
-    console.log('\n\nstart walk')
-    console.log('target=', target)
-    console.log('source=', source)
-    if (JSON.stringify(target)  == JSON.stringify(source)) {
-        console.log('match!!!')
+    // console.log('\n\nstart walk')
+    // console.log('target=', target)
+    // console.log('source.key=', source.key)
+    if (isMatch(target, source)) {
+        target.key = source.key
+        console.log('set key !!!')
         return 'match'
     } else if (source.children === undefined) {
-        console.log('none...')
+        // console.log('none...')
         return
     }
     for (const child of source.children) {
-        console.log('\nvdom=', child)
+        // console.log('\nvdom=', child)
         result = walk(target, child)
         if(result != undefined) {
-            return 'match'
+            return
         }
     }
 }
 
-function matchChildren(target, source) {
-
+function isMatch(target, source) {
+    // 参照値渡しになってしまうのでコピー
+    copiedSource = JSON.parse(JSON.stringify(source))
+    setKeyUndefined(copiedSource)
+    return JSON.stringify(target) === JSON.stringify(copiedSource)
 }
 
-function diffChildren(a, b, patch, apply, index) {
-    // console.log('----diffChildren-----')
-    // console.log('diffChildren: patch =', patch)
-    // console.log('diffChildren: apply =', apply)
-    var aChildren = a.children
-    var orderedSet = reorder(aChildren, b.children)
-    // console.log('diffChildren: orderedset=', orderedSet)
-    var bChildren = orderedSet.children
-    // console.log('diffChildren: bChildren=', bChildren)
-    var aLen = aChildren.length
-    var bLen = bChildren.length
-    var len = aLen > bLen ? aLen : bLen
-
-    // console.log('diffChildern: before for')
-    for (var i = 0; i < len; i++) {
-        // console.log('diffChildern: for')
-        var leftNode = aChildren[i]
-        var rightNode = bChildren[i]
-        // console.log('diffChildren: leftNode=', leftNode)
-        // console.log('diffChildren: rightNode=', rightNode)
-        index += 1
-
-        if (!leftNode) {
-            // VPatch.MOVEの可能性 <---
-            // 最後の子が移動した場合
-            // どこから来たか
-            if (rightNode) {
-               // console.log('diffChildren: appendPatch')
-                // Excess nodes in b need to be added
-                apply = appendPatch(apply,
-                    new VPatch(VPatch.INSERT, null, rightNode))
-            }
-        } else {
-           // console.log('diffChildren: walk')
-            walk(leftNode, rightNode, patch, index)
-        }
-
-        if (isVNode(leftNode) && leftNode.count) {
-            index += leftNode.count
+function setKeyUndefined(vdom) {
+    if ('key' in vdom) {
+        vdom.key = undefined
+        delete vdom.properties.key
+    }
+    if ('children' in vdom) {
+        for (const child of vdom.children) {
+            setKeyUndefined(child)
         }
     }
-    // console.log('diffChildern: after for')
-
-    if (orderedSet.moves) {
-       // console.log('orderedSet.moves')
-        // Reorder nodes last
-        apply = appendPatch(apply, new VPatch(
-            VPatch.ORDER,
-            a,
-            orderedSet.moves
-        ))
-    }
-
-    return apply
 }
+
 
 module.exports = setKey;
